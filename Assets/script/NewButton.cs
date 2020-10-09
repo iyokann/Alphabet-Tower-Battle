39cde868ac58c13207576840fb6b4b04d6cb26aa
iyokann <batlesplits2016@stu.kanazawa-u.ac.jp>
2020-10-09 11:29:16 +0900

    ATB

---
+++ Assets/script/NewButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewButton : MonoBehaviour
+{
+    public void LoadingNewScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    
+}
---
+++ Assets/script/Out.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Out : MonoBehaviour
+{
+    public static bool isCollision;//衝突検出変数
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isCollision = false;
+        SceneManager.LoadScene("GameOver");
+    }
+}
